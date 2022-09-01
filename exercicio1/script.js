42785eced52e84b14be49812c065f964a7decee0
francinaldapmfaria <portela854@gmail.com>
2022-09-01 14:03:43 -0300

    exercício 1 array realizado

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+// o primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+// O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+
+const numero = [10,50,34,29,42,17]
+console.log(numero)
+
+// - O segundo array deve possuir apenas **strings**;
+
+const sobremesas = ["pudim", "sorvete", "chocolate", "torta" ]
+console.log(sobremesas)
+
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+
+const diversos = [17,24, "árvore", "flores", true, false]
+console.log(diversos)
+
